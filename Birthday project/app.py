8fe9fa46da7074ab75ca1f3e8fe9a844660ae427
Abhay <abhaystudy225@gmail.com>
2026-03-16 23:47:10 +0530

    Add files via upload

---
+++ Birthday project/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ["GET", "POST"])
+def index():
+
+    if request.method == "POST":
+        user = request.form["nm"]
+        bdate = request.form["bdt"]
+
+        if bdate == "2007-02-04":
+            return render_template("b1.html", name = user)
+        else:
+            return render_template("b2.html")
+    else:
+        return render_template("wlcm.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
